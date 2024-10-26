c71b42a620a23f10f5c8b7ce087be5cf38ce0e74
Yaqin Hasan <62781980+yaq1n0@users.noreply.github.com>
2024-10-26 01:34:10 +0100

    refactor

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-# program entry point, might be needed for tkinter?
+
+import logging
+import pystray
+from PIL import Image
+from lib.runner import Runner
+
+
+logger = logging.getLogger(__name__)
+
+runner : Runner = Runner(logger) # pass primary logger
+
+icon = Image.open("lib/resources/icon.png")
+
+def onClick(_app : pystray.Icon, item : pystray.MenuItem) -> object: return runner.run(item.text, [])
+
+def quit(app : pystray.Icon, _item : pystray.MenuItem) -> None: app.stop()
+
+menu = pystray.Menu(
+    pystray.MenuItem("Hello World", onClick),
+    pystray.MenuItem("Hi World", onClick),
+    pystray.Menu.SEPARATOR,
+    pystray.MenuItem("Start Backup", onClick),
+    pystray.Menu.SEPARATOR,
+    pystray.MenuItem("Select Source Directory", onClick),
+    pystray.MenuItem("Select Target Directory", onClick),
+    pystray.Menu.SEPARATOR,
+    pystray.MenuItem("Settings", onClick),
+    pystray.Menu.SEPARATOR,
+    pystray.MenuItem("Quit", quit)
+)
+
+app = pystray.Icon("Application", icon, menu=menu)
+
+app.run()
